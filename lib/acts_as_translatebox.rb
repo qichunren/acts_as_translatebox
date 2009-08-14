cedaec231ec54f60ab473e4607005c0808a66da6
Qichunren <whyruby@gmail.com>
2009-08-14 21:33:08 +0800

    fix some ugly code and make pop translate box look well

--- lib/acts_as_translatebox.rb
+++ lib/acts_as_translatebox.rb
@@ -2,25 +2,18 @@
 module ActsAsTranslatebox
   
   def self.included(base)
-    #base.send :class_inheritable_hash, :unicorn_options
-    #base.unicorn_options = {}
-    
-    #base.send :helper_method,  :unicorn_html
-    
     base.send :include, InstanceMethods
     base.send :extend,  ClassMethods
   end
 
   module ClassMethods
     def acts_as_translatebox(*args)
-      self.unicorn_options = {:link_text => args.shift} if args.first.is_a?(String)
       after_filter :insert_translatebox_html, (args.first||{})
     end
   end
   
   module InstanceMethods
     def insert_translatebox_html
-      #oh no string concatination. my unicorns aren't scaling
       translatebox_html = <<-HTML
 <script type="text/javascript" src="http://www.google.com/jsapi"></script>
 <script type="text/javascript">
@@ -43,7 +36,7 @@ function translate_it(){
 		}
     </script>
 <style type="text/css">
-  div#chat_button {
+  div#translate_button {
     text-align: right;
     position: fixed;
     bottom: 0px;
@@ -54,7 +47,7 @@ function translate_it(){
     vertical-align: middle;
     background-color: #eee;
 }
-div#chat_input {
+div#translate_input {
     position: fixed;
     bottom: 25px;
     padding:5px;
@@ -62,33 +55,37 @@ div#chat_input {
     border:1px solid #BDBDBD;
     background:#E0E0E0;
 }
-* html div#chat_input {
+div#translate_input span{
+display:block;float:left;margin-right:4px;
+border:1px solid #FF9900;color:#FF9900;
+cursor:pointer;
+}
+* html div#translate_input {
     position: absolute;
     right: auto; bottom: auto;
-    left: expression( ( 0 - chat_input.offsetWidth + ( document.documentElement.clientWidth ? document.documentElement.clientWidth : document.body.clientWidth ) + ( ignoreMe2 = document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft ) ) + 'px' );
-    top: expression( ( -25 - chat_input.offsetHeight + ( document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight ) + ( ignoreMe = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop ) ) + 'px' );
+    left: expression( ( 0 - translate_input.offsetWidth + ( document.documentElement.clientWidth ? document.documentElement.clientWidth : document.body.clientWidth ) + ( ignoreMe2 = document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft ) ) + 'px' );
+    top: expression( ( -25 - translate_input.offsetHeight + ( document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight ) + ( ignoreMe = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop ) ) + 'px' );
 }
 
-* html div#chat_button {
+* html div#translate_button {
     position: absolute;
     right: auto; bottom: auto;
-    left: expression( ( 0 - chat_button.offsetWidth + ( document.documentElement.clientWidth ? document.documentElement.clientWidth : document.body.clientWidth ) + ( ignoreMe2 = document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft ) ) + 'px' );
-    top: expression( ( 0 - chat_button.offsetHeight + ( document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight ) + ( ignoreMe = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop ) ) + 'px' );
+    left: expression( ( 0 - translate_button.offsetWidth + ( document.documentElement.clientWidth ? document.documentElement.clientWidth : document.body.clientWidth ) + ( ignoreMe2 = document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft ) ) + 'px' );
+    top: expression( ( 0 - translate_button.offsetHeight + ( document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight ) + ( ignoreMe = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop ) ) + 'px' );
 }
 
   </style>
-  <div id="chat_input" style="display:none;">
-   <form>
+  <div id="translate_input" style="display:none;">
+  <span onclick="document.getElementById('translate_input').style.display='none'">X</span>
      <input id='words' type="text" style="width:280px;vertical-align:middle;"/>
      <input type='button' value='翻译' style="vertical-align:middle;" onclick="translate_it()"/>
-     </form>
-    <div id="target" style="width:280px;height:80px;border:1px solid #FF9900;background:#FFF;" ></div>
+    <div id="target" style="margin-top:4px;width:330px;height:120px;border:1px solid #FF9900;background:#FFF;overflow:scroll" ></div>
   </div>
-  <div id="chat_button">
-    <a href="#" style="text-decoration:none;color:black" onclick="$('chat_input').toggle();return false;">翻译助手</a>
+  <div id="translate_button">
+    <a href="#" style="text-decoration:none;color:black" onclick="document.getElementById('translate_input').style.display='';return false;">翻译助手</a>
   </div>      
       HTML
-      response.body = response.body.to_s + translatebox_html 
+      response.body = response.body.to_s + translatebox_html
     end
   end
   
